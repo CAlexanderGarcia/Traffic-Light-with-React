41b4834f36524645c292131062440aa5301f377c
CAlexanderGarcia <calexandergarcia@gmail.com>
2021-10-22 11:07:39 +0000

    Detalles Terminados

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -12,26 +12,30 @@ const Home = () => {
 	};*/
 
 	return (
-		<div className="traffic-light ">
-			<div
-				className={bright == "red" ? "light red glow" : "light red"}
-				onClick={() => {
-					setBright("red");
-				}}></div>
-			<div
-				className={
-					bright == "yellow" ? "light yellow glow" : "light yellow"
-				}
-				onClick={() => {
-					setBright("yellow");
-				}}></div>
-			<div
-				className={
-					bright == "green" ? "light green glow" : "light green "
-				}
-				onClick={() => {
-					setBright("green");
-				}}></div>
+		<div className="vl">
+			<div className="traffic-light ">
+				<div
+					className={bright == "red" ? "light red glow" : "light red"}
+					onClick={() => {
+						setBright("red");
+					}}></div>
+				<div
+					className={
+						bright == "yellow"
+							? "light yellow glow"
+							: "light yellow"
+					}
+					onClick={() => {
+						setBright("yellow");
+					}}></div>
+				<div
+					className={
+						bright == "green" ? "light green glow" : "light green "
+					}
+					onClick={() => {
+						setBright("green");
+					}}></div>
+			</div>
 		</div>
 	);
 };
